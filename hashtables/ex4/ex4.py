94aebb74b9c953d73d0b92955485c922e7efe78a
Lauren Emick <laurenemick6@gmail.com>
2020-10-01 13:02:55 -0700

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,29 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    positives = []
+    result = []
+
+    # iterate through numbers in array
+    for num in a:
+        # if num is positive
+        if num >= 0:
+            # make it negative and add it to positive array
+            num = -1 * num
+            positives.append(num)
+        # otherwise add the negatives to cache
+        else:
+            cache[num] = 0
+    
+    # iterate through numbers in "positives"
+    for num in positives:
+        # if num exists in cache
+        if num in cache:
+            # convert back to positive
+            num = -1 * num 
+            # then add it to results array
+            result.append(num)
     
     return result
 
